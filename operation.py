fee78d797adbe2263620762aa327555b0bc2d653
Uje Empress Adamaigbo <160482196+empress28@users.noreply.github.com>
2024-05-27 19:09:51 +0100

    added operations.py

---
+++ operation.py
@@ -0,0 +1,148 @@
+#addition
+# number_1 =4
+
+# number_2 =6
+
+# print(number_1 + number_2)
+
+# number_sum=(number_1 + number_2)
+
+# print(number_sum)
+
+
+
+
+# subtraction 
+
+# number_3 =10
+
+# number_4 =5
+
+# number_subtraction= number_3 - number_4
+
+# print(number_subtraction) 
+
+
+
+
+# multiplication
+
+# number_6 =5
+
+# number_7 =10
+
+# number_multiplication = number_6 * number_7
+
+# print(number_multiplication)
+
+
+
+# division
+
+# number_8 =100
+
+# number_9 =25
+
+# number_division =number_8 / number_9
+
+# print(number_division) 
+ 
+
+# phythagoras theorem  
+
+
+# opposite =float(input("Enter the height of the triangle: ")) 
+
+# adjacent =float(input("Enter the base of the traingle: "))
+
+# hypotenuse =  (opposite**2 + adjacent**2) ** (1/2)
+
+
+# print(float(hypotenuse))
+
+
+
+# phythagoras theorem
+
+# opposite = int(input("Enter the height: "))
+# adjacent =int(input("Enter the base: "))
+
+# hypotenuse = (pow(opposite,2) + pow(adjacent,2)) **(1/2) 
+
+
+# print (hypotenuse)
+
+# area of a rectangle
+
+# length=10
+
+# breadth=50
+
+# area= length * breadth
+
+# print(area)
+
+
+
+# import math
+
+# a=5
+
+# b=12
+
+# c = math.sqrt(a**2 + b**2)
+
+# print(c) 
+
+
+
+# area of a triangle
+
+# height=20
+
+# base=50
+
+# area= base * height * 1/2
+
+# print(area) 
+
+
+
+# print("Hello World") 
+
+# a_tuple=(1,2,3,4,5,6,7,8,9, 10,"button") 
+
+# print(a_tuple[10]) 
+
+
+# a_list=[1,2,3,4,5,6,7, "get started"] 
+
+
+# a_list[5]= 8
+
+# print(a_list)
+
+
+ 
+# a_set={3,5,7,9,"yaba"} 
+
+# print(a_set) 
+
+
+
+# a_set= {10,20,30,40,50, "lagos"}
+
+# print(a_set)
+
+# a_dictionary={"match":"football","game" :"ps"}
+
+# print(a_dictionary["game"]) 
+
+
+
+
+ 
+    
+
+
+
